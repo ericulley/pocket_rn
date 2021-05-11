d47413fce62a04b79a4ea0afceb29a1ea346521e
Your Name <you@example.com>
2021-05-10 20:24:57 -0600

    init

---
+++ fb-config.ts
@@ -0,0 +1,21 @@
+import firebase from "firebase/app";
+import 'firebase/functions';
+
+var firebaseConfig = {
+    apiKey: "AIzaSyB7F8Cte8I28tQbIirBZP2Beol8nFW4MaM",
+    authDomain: "pocket-rn-d0d8c.firebaseapp.com",
+    projectId: "pocket-rn-d0d8c",
+    storageBucket: "pocket-rn-d0d8c.appspot.com",
+    messagingSenderId: "362073865392",
+    appId: "1:362073865392:web:b807df31c47b0363a60f75"
+  };
+
+const app = firebase.initializeApp(firebaseConfig);
+
+const firebaseFunctions = app.functions();
+firebaseFunctions.useEmulator('localhost', 5001);
+
+export async function helloWorld(): Promise<void> {
+  const res = await firebaseFunctions.httpsCallable('helloWorld')({});
+  console.log(res);
+}
---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Pocket RN Scheduling</h1>
+    </div>
+  );
+}
+
+export default App;
